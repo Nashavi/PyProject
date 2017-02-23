366e099a6bf6c35db472d6f350fc33400ee9d32d
Avi <avinash.kamath@ucdenver.edu>
2017-02-22 17:26:37 -0800

    commit

--- kmeans.py
+++ kmeans.py
@@ -1,30 +1,27 @@
 from numpy import random, array
 
-def createcluster(N,k) :
-    random.seed(10)
-    pointspercluster = float(N/k)
-    x = []
-    for i in range (k):
-        incomecenter=random.uniform(20000,200000)
-        agecentroid=random.uniform(20,70)
-        for j in range(int(pointspercluster)):
-            x.append([random.normal(incomecenter,10000), random.normal(agecentroid,2)])
-    x= array(x)
-    return x
-
+def cluster(N,k):
+    d=[]
+    for i in range(k):
+        icenter = random.uniform(10000,100000)
+        acenter = random. uniform(20,80)
+        for j in range(int(float(N/k))):
+            d.append([random.normal(icenter,20000),random.normal(acenter,2)])
+    d = array(d)
+    return d
 
 from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import scale
+import matplotlib.pyplot as plt
 
-data = createcluster(100,7)
+dat = cluster(1000,12)
 
-model = KMeans(n_clusters=7)
+model = KMeans(n_clusters=12)
 
-model= model.fit(scale(data))
+model = model.fit(scale(dat))
 
 print model.labels_
 
-plt.figure(figsize=(8,6))
-plt.scatter(data[:,0],data[:,1],c=model.labels_.astype(float))
+plt.scatter(dat[:,0],dat[:,1],c=model.labels_.astype(float))
 plt.show()
+
