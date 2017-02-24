5411c160a5ea0a7dae73644c2d28d5103f03dcb9
Nashavi <avinash.kamath@ucdenver.edu>
2017-02-24 02:28:14 -0800

    all done

--- bsoup.py
+++ bsoup.py
@@ -2,10 +2,10 @@ import bs4 as bs
 import urllib2
 
 # artist = 'u/u2'
-# firstothersong = "11 O'Clock Tick Tock"
+# firstothertitle = "11 O'Clock Tick Tock"
 
 artist = 'c/coldplay'
-firstothersong = "1.36"
+firstothertitle = "1.36"
 
 
 artisturl = 'http://www.azlyrics.com/' + artist +'.html'
@@ -14,19 +14,19 @@ artisthome = urllib2.urlopen(artisturl)
 
 soup = bs.BeautifulSoup(artisthome,'html.parser')
 
-songlists= soup.body.find(id = "listAlbum")
+titlelists= soup.body.find(id = "listAlbum")
 
 albums = soup.body.find_all('div', class_='album')
 
-songs = songlists.find_all('a')
+titles = titlelists.find_all('a')
 
 c = 0
-for song in songs[1:]:
-    if (song.text) == firstothersong:
+for title in titles[1:]:
+    if (title.text) == firstothertitle:
         c = c + 1
         print " "
-    if (song.text.strip()) != '':
-        print (song.text.replace (",",".")) + ", " + (albums[c].text) + ", " + (song['href'].replace("..","http://www.azlyrics.com"))
+    if (title.text.strip()) != '':
+        print (title.text.replace (",",".")) + ", " + (albums[c].text) + ", " + (title['href'].replace("..","http://www.azlyrics.com"))
     else:
         print " "
         c = c + 1
