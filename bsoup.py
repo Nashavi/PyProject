4a082d918dcc410bb6714940c64e35c09e3e6c52
Nashavi <avinash.kamath@ucdenver.edu>
2017-02-24 00:48:44 -0800

    all done

--- bsoup.py
+++ bsoup.py
@@ -1,20 +1,40 @@
 import bs4 as bs
 import urllib2
 
+# artist = 'u/u2'
+# firstothersong = "11 O'Clock Tick Tock"
+
 artist = 'c/coldplay'
+firstothersong = "1.36"
+
 
 artisturl = 'http://www.azlyrics.com/' + artist +'.html'
 
 artisthome = urllib2.urlopen(artisturl)
 
-soup = bs.BeautifulSoup(artisthome,'lxml')
+soup = bs.BeautifulSoup(artisthome,'html.parser')
 
 songlists= soup.body.find(id = "listAlbum")
 
-for link in songlists.find_all('a'):
-    print (link.text)
+albums = soup.body.find_all('div', class_='album')
 
-for div in soup.body.find_all('div', class_='album'):
-    print (div.text.replace("album: ",""))
+songs = songlists.find_all('a')
+
+c = 0
+for song in songs[1:]:
+    if (song.text) == firstothersong:
+        c = c + 1
+        print " "
+    if (song.text.strip()) != '':
+        print (song.text) + ", " + (albums[c].text) + ", " + (song['href'].replace("..","http://http://www.azlyrics.com/"))
+    else:
+        print " "
+        c = c + 1
+
+
+
+
+
+
 
 
