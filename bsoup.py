043f1b47a39d6059a5e8f5431caf5c2ef1b83be4
Avi <avinash.kamath@ucdenver.edu>
2017-02-23 16:27:02 -0800

    commit

--- bsoup.py
+++ bsoup.py
@@ -1,7 +1,7 @@
 import bs4 as bs
 import urllib2
 
-artist = 'u/u2'
+artist = 'c/coldplay'
 
 artisturl = 'http://www.azlyrics.com/' + artist +'.html'
 
@@ -14,3 +14,8 @@ songlists= soup.body.find(id = "listAlbum")
 for link in songlists.find_all('a'):
     #print (link.get('href'))
     print (link.text)
+
+for div in soup.body.find_all('div', class_='album'):
+    print div.b
+
+
