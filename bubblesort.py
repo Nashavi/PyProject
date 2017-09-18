b48167a736435e4ce01c13883ee9876a9ae459d2
Nashavi <avinash.kamath@ucdenver.edu>
2017-09-18 00:15:02 -0700

    bubble sorter

---
+++ bubblesort.py
@@ -0,0 +1,15 @@
+def bubblesort(x):
+    if len(x) < 2:
+        return x
+    i = 1
+    while i < len(x):
+        while x[i-1] > x[i] and i > 0:
+            y = x[i]
+            x[i] =x[i-1]
+            x[i - 1] = y
+            i -= 1
+        i += 1
+    return x
+
+
+print(bubblesort([2,4,1,3,7,5,4,6,9,3,1,5]))
