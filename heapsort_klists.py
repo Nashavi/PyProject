3635630724c9a932f7aa56cff04f7ccc1f97c6e2
Nashavi <avinash.kamath@ucdenver.edu>
2017-09-19 01:28:10 -0700

    heapsorter

--- heapsort_klists.py
+++ heapsort_klists.py
@@ -6,8 +6,6 @@ def mergeKArray(*lists):
     r = []
     for k, arr in enumerate(lists):
         heapq.heappush(h, (arr[0], 0, k))
-    print(h)
-    print()
     while h:
         # min is the minimum element
         # i is the index of the min in the k-th array
@@ -17,7 +15,6 @@ def mergeKArray(*lists):
         if i < len(lists[k]) - 1:
             i += 1
             heapq.heappush(h, (lists[k][i], i, k))
-        print(h)
     return r
 
 print(mergeKArray([1,3,9],[12,16,18],[5,7,11],[4,5,5,6,14,19,25]))
