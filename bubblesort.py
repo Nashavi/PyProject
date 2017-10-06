15e37b44294eba41a11130026eeb323fd6b72918
Avi <avinash.kamath@ucdenver.edu>
2017-10-06 12:14:24 -0700

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,13 +1,9 @@
 def bubblesort(x):
-    if len(x) < 2:
-        return x
-    i = 1
-    while i < len(x):
-        while x[i-1] > x[i] and i > 0:
-            x[i],x[i-1] = x[i-1],x[i]
-            i -= 1
-        i += 1
-    return x
-
-
+    for i in range(len(x)):
+        for j in range(len(x) - 1 -i):
+            if x[j] > x[j+1]:
+                x[j],x[j+1] = x[j+1],x[j]
+            print(x)
+    print(x)
+    
 print(bubblesort([2,4,1,3,7,5,4,6,9,3,1,5]))
