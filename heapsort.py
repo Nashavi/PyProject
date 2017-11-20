ff2822072f8f23b4e454bddc45d3acc6bb84a0b4
Avi <avinash.kamath@ucdenver.edu>
2017-11-19 23:59:11 -0800

    Delete heapsort.py

--- heapsort.py
+++
@@ -1,8 +0,0 @@
-def heapsort(items):
-    import heapq
-    h = []
-    for item in items:
-        heapq.heappush(h,item)
-    return [heapq.heappop(h) for k in range(len(h))]
-
-print(heapsort([5,3,1,4,6,9,8]))
